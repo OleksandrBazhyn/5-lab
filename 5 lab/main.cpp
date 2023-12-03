7edaf8d186c23b06c7a369a9da5f4572ba47499a
OleksandrBazhyn <oleksandr_bazhyn@knu.ua>
2023-12-03 18:16:19 +0200

    added ocyncstream output

--- 5 lab/main.cpp
+++ 5 lab/main.cpp
@@ -4,16 +4,33 @@
 // Усі обчислення : A1, A2, A3, A4, B1, B2, C
 // “Повільні” обчислення : A1, A3
 // Залежності : B1(A1, A2), C(B1, B2)
+// 
+// Транзитивно залежність C можна представити як C(A1, A2, B2)
 
 #include <iostream>
 #include <future>
+#include <syncstream>
 
-void work()
+void slow(const std::string& name)
 {
 
 }
 
+void quick(const std::string& name)
+{
+
+}
+
+void work()
+{
+	std::osyncstream synced_out(std::cout);
+
+	synced_out << "\nWork done!\n";
+}
+
 int main()
 {
+	work();
+
 	return 0;
 }
