3633e0b65fc5dc455cc855765d121a1855706dd5
OleksandrBazhyn <oleksandr_bazhyn@knu.ua>
2023-12-03 19:05:52 +0200

    completed

--- 5 lab/main.cpp
+++ 5 lab/main.cpp
@@ -11,26 +11,49 @@
 #include <future>
 #include <syncstream>
 
+std::osyncstream synced_out(std::cout);
+
 void slow(const std::string& name)
 {
-
+	synced_out << "Slow computation " << name << '\n';
+	std::this_thread::sleep_for(std::chrono::seconds(7));
 }
 
 void quick(const std::string& name)
 {
-
+	synced_out << "Quick computation " << name << '\n';
+	std::this_thread::sleep_for(std::chrono::seconds(1));
 }
 
 void work()
 {
-	std::osyncstream synced_out(std::cout);
+	std::future<void> futureA1 = std::async(std::launch::async, slow, "A1");
+	std::future<void> futureA2 = std::async(std::launch::async, quick, "A2");
+	std::future<void> futureA3 = std::async(std::launch::async, slow, "A3");
+	std::future<void> futureA4 = std::async(std::launch::async, quick, "A4");
+
+	futureA1.wait();
+	futureA2.wait();
+	std::future<void> futureB1 = std::async(std::launch::deferred, quick, "B1");
+	std::future<void> futureB2 = std::async(std::launch::deferred, quick, "B2");
+
+	futureB1.wait();
+	futureB2.wait();
+	std::future<void> futureC = std::async(std::launch::deferred, quick, "C");
+
+	futureC.get();
 
 	synced_out << "\nWork done!\n";
 }
 
 int main()
 {
+	auto start = std::chrono::steady_clock::now();
 	work();
+	auto end = std::chrono::steady_clock::now();
+
+	synced_out << "Total time: " << std::chrono::duration_cast<std::chrono::seconds>(end - start).count() << " seconds" << '\n';
+
 
 	return 0;
 }
